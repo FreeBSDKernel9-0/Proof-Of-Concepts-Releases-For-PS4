2cba3787692d7a00338d3956e34d719be35ad9d9
FreeBSDKernel9-0 <coconutrepairs@gmail.com>
2026-03-31 22:45:37 +0100

    My last subfolder thing for this project.

---
+++ research/leak-pointer.js
@@ -0,0 +1,38 @@
+let confuser = { a: 1.1 }; 
+let trash = []; 
+
+function leakHunter() {
+    for (let i = 0; i < 10000; i++) {
+        
+        confuser.a = (i % 2 === 0) ? { leak: 0x41414141 } : 1.2345;
+        
+        
+        if (i % 100 === 0) trash.push(new Array(100)); 
+    }
+
+    let result = confuser.a;
+    
+    
+    if (typeof result !== 'object' && typeof result !== 'number') {
+        console.log("[!] LEAK LOOKUP (Yes): " + result);
+        return true;
+    }
+    return false;
+}
+
+function runExperiment() {
+    
+    let found = leakHunter();
+    
+    if (found) {
+        
+        console.log("[!!!] FOUND IT! (checkLog)");
+    } else {
+        
+    
+        setTimeout(runExperiment, 100); 
+    }
+}
+
+
+runExperiment();
